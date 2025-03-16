29d26fab86d242137da66f90ccc7b3492519a3dd
janhavi.rai <janhavi.rai@cleartrip.com>
2025-03-16 19:27:47 +0530

    added prod

---
+++ src/main/java/com/cleartrip/ecommerce_management_project/Entity/Product.java
@@ -0,0 +1,34 @@
+package com.cleartrip.ecommerce_management_project.Entity;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "products")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+    @Column(nullable = false)
+    private Double price;
+
+    @Column(nullable = false)
+    private String category;
+
+    // one product can have one inventory
+    @JsonManagedReference
+    @OneToOne(mappedBy = "product", cascade = CascadeType.ALL)
+    private Inventory inventory;
+}
